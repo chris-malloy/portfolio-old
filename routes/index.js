d41b77bb102bdc01168c5e452594cca9bd248810
chris-malloy <chrismalloymusic@gmail.com>
2017-11-20 14:21:33 -0500

    temp page

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('index', { title: "Chris Malloy's Portfolio" });
 });
+/* GET temporary */
+router.get('/temporary', function(req, res, next) {
+    res.render('temporary', { title: "Chris Malloy's Portfolio" })
+});
 
 module.exports = router;
