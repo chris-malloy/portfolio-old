3182b4a87189f6560bb8076fa9998db8bfadcf88
chris-malloy <chrismalloymusic@gmail.com>
2017-11-27 15:31:32 -0500

    change title

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mainTitle = "Chris Malloy's Portfolio";
+const mainTitle = "Chris Malloy";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
